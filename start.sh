5d7329a1c2e7702d64c3417e1f2fab7e2eec4610
egortsaryk9 <egortsaryk@gmail.com>
2022-09-13 23:23:16 +0200

    Logs fixed

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ echo "Creating appliction volumes ..."
 docker volume create --name=chainstorage && docker volume create --name=offchainstorage && docker volume create --name=filestorage && docker volume create --name=logstorage && docker volume create --name=zookeeper_data && docker volume create --name=kafka_data
 echo "Starting appliction containers ..."
 docker-compose up -d --force-recreate
-echo "Waiting for Vedai application containers to start ..."
+echo "Waiting for Casimir application containers to start ..."
 sleep 3
 # echo "Installing npm packages for a setup script ..."
 # npm install
